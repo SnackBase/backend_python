8a5a08ac215b5ce9168e96e916559974196f984a
David Schüler <david.leon.sch@gmail.com>
2026-01-01 16:53:54 +0100

    :sparkles: set up main functionality

---
+++ app/__main__.py
@@ -0,0 +1,8 @@
+import uvicorn
+
+from app import app
+from settings import get_settings
+
+if __name__ == "__main__":
+    settings = get_settings()
+    uvicorn.run(app, host="0.0.0.0", port=settings.port)
---
+++ app/main.py
@@ -0,0 +1,42 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# from app.data.sql import create_db_and_tables
+from app.settings import get_settings
+
+settings = get_settings()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # create_db_and_tables()
+    yield
+
+
+app = FastAPI(
+    title="Backend API for DrinkBar",
+    description="This API provides database connectivity and database validation.",
+    root_path="/api/v1",
+    lifespan=lifespan,
+    swagger_ui_init_oauth={  # see: https://swagger.io/docs/open-source-tools/swagger-ui/usage/oauth2/
+        "appName": "DrinkBar",
+        "clientId": settings.auth_client_id,
+        "clientSecret": settings.auth_client_secret,
+        "usePkceWithAuthorizationCodeGrant": True,
+        "scopes": "",
+    },
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# app.include_router(products_router)
