4ad70480d658d2b31641bb446f0846f2c2d893c3
David Schüler <david.leon.sch@gmail.com>
2026-01-04 21:13:24 +0100

    🚸 applied filter to only show customers, no kiosk or admin user

--- app/data/access/user.py
+++ app/data/access/user.py
@@ -4,5 +4,16 @@ from app.data.models.user import User
 
 
 async def get_users_data() -> list[User]:
-    users: list[dict] = keycloak_admin.get_users({})
+    # First get the group
+    groups: list[dict] = keycloak_admin.get_groups({"search": "customer"})
+    if not groups or len(groups) < 1:
+        return []
+    group_id = groups[0].get("id", None)
+    if group_id is None:
+        return []
+
+    # Get all members of the group
+    users = keycloak_admin.get_group_members(group_id=group_id)
+
+    print(users)
     return [User(**u) for u in users]
